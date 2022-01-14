3733fc0c695377a5dd3932d42af5d576f6282ee9
MagicCat00 <vsggdfaf@gmail.com>
2022-01-14 12:41:32 +0300

    функция конструктор

--- main.js
+++ main.js
@@ -1,46 +1,14 @@
 'use strict'
 
-const worker = {
-    workplace: 1,
-    dinner: 1,
-    goToWork: function () {
-        console.log('Иду на работу');
-    },
-    leaveWork: function () {
-        console.log('Ухожу с работы');
-    },
-    work: function () {
-        console.log('Работаю');
-    },
-    sayHello: function () {
-        console.log('Привет, меня зовут ' + this.name);
-    }
+const Person = function (name) {
+    this.name = name
 }
+//конструктор, с большой буквы
 
-const frontEndDev = Object.create(worker)
-const backEndDev = Object.create(worker)
+const person1 = new Person('Vlad')
+const person2 = new Person('Alex')
+const person3 = new Person('Anna')
 
-frontEndDev.dinner = 2
-
-frontEndDev.role = 'Front End Developer'
-backEndDev.role = 'Back End Developer'
-
-frontEndDev.work = function () {
-    console.log('Пишу фронт, ем печеньки');
-}
-
-frontEndDev.work = function () {
-    console.log('Мучаюсь с Бд');
-}
-
-const developer1 = Object.create(frontEndDev)
-const developer2 = Object.create(frontEndDev)
-const developer3 = Object.create(backEndDev)
-const developer4 = Object.create(backEndDev)
-
-developer1.name = 'Alex'
-developer2.name = 'Ivan'
-developer3.name = 'Anna'
-developer4.name = 'Elena'
-
-console.log(frontEndDev.isPrototypeOf(developer3));
+console.log(person1);
+console.log(person2);
+console.log(person3);
